960372be3ee7d452fd90dc6c58a5e276a1cdd7bb
Trevor <trevoralternate@gmail.com>
2018-03-13 17:00:54 -0600

    Added really basic commandline options to generate a standup report for today or tomorrow

--- Assistant/Assistant/Program.cs
+++ Assistant/Assistant/Program.cs
@@ -15,6 +15,86 @@ namespace Assistant
     {
         static void Main(string[] args)
         {
+            bool continueRunning = true;
+
+            if(args.Length != 0)
+            {
+                var suppliedArgs = parseArguments(args);
+                if(suppliedArgs.Contains("standup"))
+                {
+                    continueRunning = false;
+                    if(suppliedArgs.Contains("tomorrow"))
+                    {
+                        // run tomorrow
+                        generateStandup(1);
+                    }
+                    else
+                    {
+                        // run today
+                        generateStandup();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized command. Please type \"help\" for command line options");
+                }
+            }
+
+            while (continueRunning)
+            {
+                Console.WriteLine("Enter a command or \"help\"");
+                string input = Console.ReadLine();
+
+                List<string> inputArgs = parseArguments(input);
+                if(inputArgs.Contains("exit"))
+                {
+                    continueRunning = false;
+                }
+                else
+                {
+                    if (inputArgs.Contains("standup"))
+                    {
+                        continueRunning = false;
+                        if (inputArgs.Contains("tomorrow"))
+                        {
+                            // run tomorrow
+                            generateStandup(1);
+                        }
+                        else
+                        {
+                            // run today
+                            generateStandup();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unrecognized command. Please type \"help\" for command line options");
+                    }
+                }
+            }
+
+            Console.WriteLine("Exiting...");
+        }
+
+        static List<string> parseArguments(string[] args)
+        {
+            List<string> argsToReturn = new List<string>();
+            foreach(string arg in args)
+            {
+                argsToReturn.Add(arg.TrimStart('-').TrimEnd(' ').ToLower());
+            }
+
+            return argsToReturn;
+        }
+        static List<string> parseArguments(string args)
+        {
+            var splitArgs = args.Split(' ');
+
+            return parseArguments(splitArgs);
+        }
+
+        static void generateStandup(int offset = 0)
+        {
             KanbanFlow kb = new KanbanFlow();
             kb.SignIn();
             kb.populateBoard();
@@ -24,7 +104,7 @@ namespace Assistant
             cs.SignIn();
 
             StandupReport report = new StandupReport(cs, kb);
-            report.GenerateReport(1);
+            report.GenerateReport(offset);
         }
     }
 }
